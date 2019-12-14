562f81e3aee33066fad2f99552d6ea4c3eeb48dd
Lazovchik <lazovchik@gmail.com>
2019-12-14 15:48:28 +0100

    Delition completed

--- public/javascripts/deleteMetric.js
+++ public/javascripts/deleteMetric.js
@@ -1,35 +1,23 @@
-/*$(".deleter").click((e) => {
-	//console.log("deleter");
-	e.preventDefault();
-	var id = $("#metricId").val();
-	console.log(id);
-});
-
-$("form"){
-	$(".deleter").click((e)=>{
-		e.preventDefault();
-		console.log("dsafsd");	
-	});
-};
-*/
-/*
-$(document).on("button" function (e) {
-	e.preventDefault();
-	concole.log("zhuzhu");
-});*/
-/*
-$(".deleter").click((e)=>{
-	e.preventDefault();
-	var id = $(e.target).closest('.form-group').find("#1").val();
-
-	console.log(id);
-
-});*/
-
 $(document).off('click').on('click', '.deleter', function(e){
-	e.preventDefault();
+	//e.preventDefault();
 	const id = $(this).closest('form').children('.item-id').val();
 	//$("body").html('<div>'+id+'</div>');
 	//console.log('id suka:');
 	console.log(id);
+	let arr = {id: id}
+
+	$.ajax({
+		url:  '/delMetric',
+		type: 'POST',
+		data: JSON.stringify(arr),
+		dataType: 'json',
+		contentType: 'application/json; charset=utf-8',
+        async: true,
+        success: function() {
+			console.log("done");
+		},
+		error: function(data, status, error){
+			console.log("error");
+		}
+	});
 });
